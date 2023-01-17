d53692cc52d0707205ae8e33b144e669d617bb97
Robert Fodor <Robert.Fodor@sluz.ch>
2023-01-17 17:32:41 +0100

    task 1 done

---
+++ Aufgaben/07-design-pattern/Payroll/AccountHolderAdapter.cs
@@ -0,0 +1,29 @@
+﻿using Payroll.ThirdParty;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Payroll
+{
+    public class AccountHolderAdapter : IAccountHolder
+    {
+        private Employable employable;
+
+        public AccountHolderAdapter(Employable employable)
+        {
+            this.employable = employable;
+        }
+
+        public string GetAccountDetails()
+        {
+            return employable.GetNote("AccountDetails");
+        }
+
+        public override string ToString()
+        {
+            return employable.ToString();
+        }
+    }
+}
--- Aufgaben/07-design-pattern/Payroll/Program.cs
+++ Aufgaben/07-design-pattern/Payroll/Program.cs
@@ -17,9 +17,11 @@ namespace Payroll
             jayneDone.AddNote("AccountDetails", "XYZ-789");
             johnDoe.AddNote("Position", "Head of HR");
 
-            // FIXME: Payroll.Employable -> Payroll.ThirdParty
-            // Banking.PayOut(johnDoe, johnDoe.GetSalary() / 12);
-            // Banking.PayOut(jayneDone, jayneDone.GetSalary() / 12);
+            AccountHolderAdapter accountJohnDoe = new(johnDoe);
+            AccountHolderAdapter accountJayneDoe = new(jayneDone);
+
+            Banking.PayOut(accountJohnDoe, johnDoe.GetSalary() / 12);
+            Banking.PayOut(accountJayneDoe, jayneDone.GetSalary() / 12);
         }
     }
 }
